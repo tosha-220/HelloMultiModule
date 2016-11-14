2e42498b711df31253d4636c204bef1db9afba2b
tosha220 <tosha-220@yandex.ru>
2016-11-14 18:44:28 +0300

    first commit with xml dependency

---
+++ call/src/main/java/com/netcraker/CallingHello.java
@@ -0,0 +1,8 @@
+package com.netcraker;
+
+public class CallingHello {
+    public static void main(String[] args) {
+        Hello hello=new Hello();
+        hello.printHello();
+    }
+}
---
+++ hello/src/main/java/com/netcraker/Hello.java
@@ -0,0 +1,7 @@
+package com.netcraker;
+
+public class Hello {
+    public void printHello(){
+        System.out.println("Hello");
+    }
+}
